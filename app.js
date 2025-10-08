20d74f97327faa8a735de2ef742d83e7e6ed44b7
Alex Rujan <alexrujan3@gmail.com>
2025-10-08 20:08:13 +0300

    bugs

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ import express from "express";
 
 import path from "path";
 import cors from "cors";
-
+import microCors from "micro-cors";
 import movieRouter from "./routes/movieRoutes.js";
 import AppError from "./utils/appError.js";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 
-app.set("trust proxy", 1); // trust first proxy
+const cors = microCors({
+  allowMethods: ["GET", "POST", "OPTIONS"],
+  origin: "*",
+});
+app.use(cors);
 
-app.use(
-  cors({
-    origin: "https://next-one-similar-movies-client.vercel.app", // frontend dev server
-    credentials: true, // permite trimiterea cookie-urilor
-  })
-);
-app.options("*", cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
