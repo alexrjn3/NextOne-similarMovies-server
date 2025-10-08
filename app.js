ece4b881efbacf881ad16bce1f09c5c92a8a8897
Alex Rujan <alexrujan3@gmail.com>
2025-10-08 19:56:28 +0300

    cors

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import express from "express";
-import cors from "cors";
+
 import path from "path";
+import microCors from "micro-cors";
 
 import movieRouter from "./routes/movieRoutes.js";
 import AppError from "./utils/appError.js";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
+
+const cors = microCors({
+  allowMethods: ["GET", "POST", "OPTIONS"],
+  origin: "*",
+});
+
+app.use(cors);
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
